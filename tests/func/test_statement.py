4a76907a1437bdbe57ead0ff1258899387388152
Christopher Kirkley <ckirkley@gmail.com>
2020-08-16 23:40:37 -0700

    Changed artist statement to include artist and statement.

--- tests/func/test_statement.py
+++ tests/func/test_statement.py
@@ -108,6 +108,8 @@ def test_returns(browser, test_client, db):
 
     table = browser.find_element_by_id('track-sales')
     rows = table.find_elements_by_tag_name('tr')
-    assert rows[1].find_element_by_id('track_name').text == ''
+    assert rows[1].find_element_by_id('track_name').text == 'Adounia'
+
+    time.sleep(2000)
 
     
