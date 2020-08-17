365913ab23da2d43ea3a9314294b876a37046bed
Christopher Kirkley <ckirkley@gmail.com>
2020-08-17 10:05:12 -0700

    Refactored to create statement summary table.

--- tests/unit/test_statements_artist.py
+++ tests/unit/test_statements_artist.py
@@ -51,26 +51,57 @@ def test_can_get_statement_recoupables_by_artist_subquery(test_client, db):
             (1, Decimal('1398.06')),
             ]
 
+def test_can_create_statement_summary_table(test_client, db):
+    date_range = '2020_01_01_2020_01_31'
+    table = ga.create_statement_summary_table(date_range)
+    assert len(db.session.query(table).all()) == 0
+    assert table.__tablename__ == 'statement_summary_2020_01_01_2020_01_31'
+
 def test_can_get_statement_by_artist_subquery(test_client, db):
     setup_statement(test_client, db)
     statement_previous_balance_by_artist = ga.create_statement_previous_balance_by_artist_subquery(1)
     statement_sales_by_artist = ga.create_statement_sales_by_artist_subquery(1)
     statement_advances_by_artist = ga.create_statement_advances_by_artist_subquery(1)
     statement_recoupables_by_artist = ga.create_statement_recoupables_by_artist_subquery(1)
+    date_range = '2020_01_01_2020_01_31'
+    table = ga.create_statement_summary_table(date_range)
+    assert len(db.session.query(table).all()) == 0
+    assert table.__tablename__ == 'statement_summary_2020_01_01_2020_01_31'
     statement_by_artist = ga.create_statement_by_artist_subquery(
                                                         statement_previous_balance_by_artist,
                                                         statement_sales_by_artist,
                                                         statement_advances_by_artist,
                                                         statement_recoupables_by_artist)
-    assert len(db.session.query(statement_by_artist).all()) > 0
-    assert db.session.query(statement_by_artist).all() == [
+    assert len((statement_by_artist).all()) > 0
+    assert statement_by_artist.all() == [
             (
                 1, # artist_id
                 Decimal('0'), # previous_balance 
-                Decimal('24.95'), # sales 
                 Decimal('1398.06'), # recoupable
+                Decimal('24.95'), # sales 
                 Decimal('9542.63'), # advance
-                Decimal('-1373.11'), # total to split
-                Decimal('-686.56'), # sales - recoupable / 2
-                Decimal('-10229.19'), # balance forward
                 )]
+
+def test_can_insert_statement_summary_data_into_table(test_client, db):
+    setup_statement(test_client, db)
+    statement_previous_balance_by_artist = ga.create_statement_previous_balance_by_artist_subquery(1)
+    statement_sales_by_artist = ga.create_statement_sales_by_artist_subquery(1)
+    statement_advances_by_artist = ga.create_statement_advances_by_artist_subquery(1)
+    statement_recoupables_by_artist = ga.create_statement_recoupables_by_artist_subquery(1)
+    date_range = '2020_01_01_2020_01_31'
+    table = ga.create_statement_summary_table(date_range)
+    statement_by_artist = ga.create_statement_by_artist_subquery(
+                                                        statement_previous_balance_by_artist,
+                                                        statement_sales_by_artist,
+                                                        statement_advances_by_artist,
+                                                        statement_recoupables_by_artist)
+    ga.insert_into_statement_summary(statement_by_artist, table)
+    assert len(db.session.query(table).all()) > 0
+    res = db.session.query(table).first()
+    assert res.id == 1
+    assert res.artist_id == 1
+    assert res.previous_balance == Decimal('0')
+    assert res.recoupables == Decimal('1398.06')
+    assert res.advances == Decimal('9542.63')
+    assert res.sales == Decimal('24.95')
+
