e1ace878a127065ca97aef9176a73d4e420a998b
Christopher Kirkley <ckirkley@gmail.com>
2020-10-24 16:44:32 -0700

    Functional test for SDS.

--- tests/func/test_sds.py
+++ tests/func/test_sds.py
@@ -41,7 +41,7 @@ def test_returns(browser, test_client, db):
     browser.find_element_by_id('income').click()
     browser.find_element_by_id('import_income').click()
 
-    path = os.getcwd() + "/tests/files/sds_digital_test1.csv"
+    path = os.getcwd() + "/tests/files/sds_test1.csv"
     browser.find_element_by_id('file_upload').send_keys(path)
     browser.find_element_by_id('source_statement').click()
     browser.find_element_by_id('sds').click()
@@ -50,22 +50,22 @@ def test_returns(browser, test_client, db):
     
     """ User sees statement added to the list of pending statements. """
     pending_statement = browser.find_element_by_id('pending_statement')
-    assert pending_statement.text == 'sds_digital_test1.csv'
+    assert pending_statement.text == 'sds_test1.csv'
 
     """ User sees prompt for errors, and clicks to fix matching errors. """
-    assert browser.find_element_by_id('track_matching_errors').text == "You have 1 track matching errors."
+    assert browser.find_element_by_id('track_matching_errors').text == "You have 10 track matching errors."
 
     browser.find_element_by_id('fix_isrc_errors').click()
 
     """ User matches version number. """
     table = browser.find_element_by_id('matching_error_table')
     rows = table.find_elements_by_tag_name('tr')
-    assert len(rows) == 2
+    assert len(rows) == 11
     
     browser.find_element_by_id('match').click()
 
-    browser.find_element_by_id('isrc_id').click()
-    browser.find_element_by_id('QZDZE1905x03').click()
+    browser.find_element_by_id('distributor').click()
+    browser.find_element_by_id('sds').click()
     browser.find_element_by_id('new_value').click()
     browser.find_element_by_id('QZDZE1905003').click()
     browser.find_element_by_id('submit').click()
@@ -77,12 +77,12 @@ def test_returns(browser, test_client, db):
     """ User goes to view imported income statements. """
     table = browser.find_element_by_id('income_table')
     rows = table.find_elements_by_tag_name('tr')
-    assert rows[1].find_elements_by_tag_name('td')[0].text == 'sds_digital_test1.csv'
+    assert rows[1].find_elements_by_tag_name('td')[0].text == 'sds_test1.csv'
     browser.find_element_by_id('view1').click()
     
     """ User goes to imported income statement detail to view summa to view summary."""
     time.sleep(1)
-    assert browser.find_element_by_id('number_of_records').text == '13'
+    assert browser.find_element_by_id('number_of_records').text == '10'
 
     """ User decides to go back and delete this statement. """
     browser.get('http://localhost:3000/income')
