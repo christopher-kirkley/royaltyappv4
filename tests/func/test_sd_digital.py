de6d0a36aef9899b64937a597ce043fd2b8fb55d
Christopher Kirkley <ckirkley@gmail.com>
2020-10-24 15:23:12 -0700

    SD digital func test passing.

--- tests/func/test_sd_digital.py
+++ tests/func/test_sd_digital.py
@@ -55,40 +55,34 @@ def test_returns(browser, test_client, db):
     """ User sees prompt for errors, and clicks to fix matching errors. """
     assert browser.find_element_by_id('track_matching_errors').text == "You have 1 track matching errors."
 
-    time.sleep(1000)
     browser.find_element_by_id('fix_isrc_errors').click()
-    
 
     """ User matches version number. """
     table = browser.find_element_by_id('matching_error_table')
     rows = table.find_elements_by_tag_name('tr')
-    assert len(rows) == 5
+    assert len(rows) == 2
     
-
     browser.find_element_by_id('match').click()
 
-    browser.find_element_by_id('column').click()
-    browser.find_element_by_id('missingupc').click()
+    browser.find_element_by_id('isrc_id').click()
+    browser.find_element_by_id('QZDZE1905x03').click()
     browser.find_element_by_id('new_value').click()
-    browser.find_element_by_id('SS-050cass').click()
+    browser.find_element_by_id('QZDZE1905003').click()
     browser.find_element_by_id('submit').click()
 
-    table = browser.find_element_by_id('matching_error_table')
-    rows = table.find_elements_by_tag_name('tr')
-    assert len(rows) == 1
-
-    """ Process payments. """
+    time.sleep(1)
+    """ Returns to process page and process payments. """
     browser.find_element_by_id('process_errors').click()
 
     """ User goes to view imported income statements. """
     table = browser.find_element_by_id('income_table')
     rows = table.find_elements_by_tag_name('tr')
-    assert rows[1].find_elements_by_tag_name('td')[0].text == 'bandcamp_test_2.csv'
+    assert rows[1].find_elements_by_tag_name('td')[0].text == 'sd_digital_test1.csv'
     browser.find_element_by_id('view1').click()
     
     """ User goes to imported income statement detail to view summa to view summary."""
     time.sleep(1)
-    assert browser.find_element_by_id('number_of_records').text == '4'
+    assert browser.find_element_by_id('number_of_records').text == '13'
 
     """ User decides to go back and delete this statement. """
     browser.get('http://localhost:3000/income')
